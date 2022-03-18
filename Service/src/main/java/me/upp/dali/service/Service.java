195861785191395a8f33fb5561d385b4ce963b72
Daligz <oscardali370@gmail.com>
2022-03-17 21:35:26 -0600

    Simple example implemented

--- Service/src/main/java/me/upp/dali/service/Service.java
+++ Service/src/main/java/me/upp/dali/service/Service.java
@@ -1,3 +1,11 @@
 package me.upp.dali.service;
 
-public class Service { }
+import spark.Spark;
+
+public class Service {
+
+    public static void main(final String[] args) {
+        Spark.port(6969);
+        Spark.get("/test", (request, response) -> "test request!");
+    }
+}
