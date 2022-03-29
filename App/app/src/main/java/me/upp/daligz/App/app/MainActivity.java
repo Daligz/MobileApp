527d3047f810a34bd4f49e7c28936a2b2976ab21
Daligz <oscardali370@gmail.com>
2022-03-28 23:19:50 -0600

    Automatic image view generator implemented

--- App/app/src/main/java/me/upp/daligz/App/app/MainActivity.java
+++ App/app/src/main/java/me/upp/daligz/App/app/MainActivity.java
@@ -1,6 +1,7 @@
 package me.upp.daligz.App.app;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,15 +10,19 @@ import me.upp.daligz.App.app.downloader.ImageDownloader;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String API_URL = "https://picsum.photos/200/300?random=1";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.runAnimations();
         ImageDownloader.download(
-                "https://estaticos.muyinteresante.es/uploads/images/gallery/594a1ced5bafe85dfd3c9869/gato-romano-redes.jpg",
+                API_URL,
                 findViewById(R.id.imageViewer)
         );
+        final Button button = (Button) findViewById(R.id.buttonLoad);
+        button.setOnClickListener(view -> ImageDownloader.download(API_URL, this));
     }
 
     private void runAnimations() {
--- App/app/src/main/java/me/upp/daligz/App/app/downloader/ImageDownloader.java
+++ App/app/src/main/java/me/upp/daligz/App/app/downloader/ImageDownloader.java
@@ -1,15 +1,28 @@
 package me.upp.daligz.App.app.downloader;
 
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.squareup.picasso.Picasso;
 
+import me.upp.daligz.App.app.R;
+
 // Utility class
 public class ImageDownloader {
+
     public static void download(final String url, final ImageView imageView) {
         Picasso
                 .get()
                 .load(url)
                 .into(imageView);
+    }
+
+    public static void download(final String url, final AppCompatActivity appCompatActivity) {
+        final ImageView imageView = new ImageView(appCompatActivity);
+        final LinearLayout linearLayout = (LinearLayout) appCompatActivity.findViewById(R.id.linear_layout);
+        linearLayout.addView(imageView);
+        ImageDownloader.download(url, imageView);
     }
 }
