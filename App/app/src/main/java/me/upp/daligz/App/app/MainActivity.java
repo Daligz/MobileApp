af33cf22095002be4e5cf9e7721ede5ed17e49e5
Daligz <oscardali370@gmail.com>
2022-03-28 22:25:24 -0600

    Animation runner implemented

--- App/app/src/main/java/me/upp/daligz/App/app/MainActivity.java
+++ App/app/src/main/java/me/upp/daligz/App/app/MainActivity.java
@@ -12,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.runAnimations();
+    }
+
+    private void runAnimations() {
         new AnimatedBackground(this).run();
     }
 }
