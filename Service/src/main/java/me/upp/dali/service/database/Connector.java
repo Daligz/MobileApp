eead30170faff3571a8b395d722d7a01b7114bb1
Daligz <oscardali370@gmail.com>
2022-04-02 15:24:34 -0600

    Connector implemented

--- Service/src/main/java/me/upp/dali/service/database/Connector.java
+++ Service/src/main/java/me/upp/dali/service/database/Connector.java
@@ -1,6 +1,9 @@
 package me.upp.dali.service.database;
 
+import me.upp.dali.service.database.tables.TablePosts;
 import net.royalmind.minecraft.plugin.minigamecluster.mysqlapi.MySQL;
+import net.royalmind.minecraft.plugin.minigamecluster.mysqlapi.queries.CreateTableQuery;
+import net.royalmind.minecraft.plugin.minigamecluster.mysqlapi.queries.Query;
 
 public class Connector {
 
@@ -18,7 +21,15 @@ public class Connector {
     }
 
     private void createTables() {
-
+        final String postsTableQuery = new CreateTableQuery(TablePosts.TABLE_NAME.getValue())
+                .ifNotExists()
+                .column(TablePosts.ID.getValue(), "VARCHAR NOT NULL")
+                .column(TablePosts.URL.getValue(), "VARCHAR NOT NULL")
+                .column(TablePosts.RATE.getValue(), "INT NOT NULL")
+                .column(TablePosts.CATEGORY.getValue(), "VARCHAR NOT NULL")
+                .primaryKey(TablePosts.ID.getValue())
+                .build();
+        new Query(this.mySQL, postsTableQuery).executeUpdateAsync();
     }
 
     public void close() {
